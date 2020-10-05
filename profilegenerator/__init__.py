17b0d239679663fdf32494000e20c0bcb7317d85
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2020-10-05 14:17:09 +0100

    profilegenerator module

---
+++ profilegenerator/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Bioschemas profile generator
+"""
+
+__author__ = "Bioschemas.org community"
+__copyright__ = """© 2020 Heriot-Watt University, UK
+© 2020 The University of Manchester, UK
+"""
+__license__ = "MIT" # https://spdx.org/licenses/MIT
+
+# Convenience export of public functions/types
+from ._version import __version__  # noqa
