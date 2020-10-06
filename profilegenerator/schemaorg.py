46b30a6f813ca4c84ebbf8352e7e5566ac0fe8c7
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2020-10-06 16:01:10 +0100

    attempt: Python types to map schema.org hierarchy

--- profilegenerator/schemaorg.py
+++ profilegenerator/schemaorg.py
@@ -15,7 +15,14 @@ __copyright__ = """© 2020 Heriot-Watt University, UK
 """
 __license__ = "MIT" # https://spdx.org/licenses/MIT
 
+from collections import OrderedDict
+
 import rdflib
+from rdflib import Dataset, URIRef
+# rdflib knows about some namespaces, like FOAF
+from rdflib.namespace import RDF, RDFS, Namespace
+SCHEMA = Namespace("http://schema.org/")
+
 from string import Template
 import logging
 from ._logging import LOG_TRACE
@@ -35,10 +42,62 @@ def load_schemaorg(schemaver="latest"):
         _logger.log(LOG_TRACE, d.serialize(format="trig").decode("utf-8"))
     return result
 
-def find_properties(schematype, profile, schemaver="latest"):
-    load_schemaorg(schemaver)
-    return [ # FIXME: hardcoded for now!
-        (schematype, []),
-        ("CreativeWork", []),
-        ("Thing", [{"name": "foo"} ])
-    ]
+class SchemaType(Type):
+    _uri2type = {}
+
+    @classmethod
+    def _as_type(cls, uri: URIRef) -> SchemaType:
+        if uri not in cls._uri2type:
+            # create same subclass (SchemaProperty or SchemaClass)
+            cls._uri2type[uri] = cls(uri, cls.dataset)
+        return cls._uri2type[uri]
+    
+    def __init__(self, uri: URIRef, dataset: Dataset)
+        self.uri = uri
+        self.dataset = dataset
+        self._uri2type[uri] = self # self-register
+        bases = list(cls.supertypes())
+        super().__init__(uri, bases, {})
+
+    # abstract
+    def supertypes(self):
+        return []
+    def label(self):
+        for label in self.dataset.objects(self.uri, RDFS.label):
+            return label
+    def comment(self):
+        for comment in self.dataset.objects(self.uri, RDFS.comment):
+            return comment
+
+class SchemaProperty(SchemaType):
+    def supertypes(self):
+        return self.dataset.objects(self.uri, RDFS.subPropertyOf)
+    
+    def domainIncludes(self):
+        return map(SchemaClass._as_type, 
+            self.dataset.objects(self.uri, RDFS.domainIncludes))        
+    
+    def rangeIncludes(self):
+        return map(SchemaClass._as_type, 
+            self.dataset.objects(self.uri, RDFS.rangeIncludes))        
+
+class SchemaClass(Type):
+    def supertypes(self):
+        return map(SchemaClass._as_type, 
+            self.dataset.objects(self.uri, RDFS.subClassOf))
+
+    def propertiesIncludingAsDomain(self):
+        return map(SchemaProperty._as_type, 
+            self.dataset.subjects(self.uri, SCHEMA.domainIncludes))
+
+    def propertiesIncludingAsRange(self):
+        return map(SchemaProperty._as_type, 
+            self.dataset.subjects(self.uri, SCHEMA.rangeIncludes))
+
+def find_properties(schematype, schemaver="latest"):    
+    s = SchemaType(SCHEMA[schematype], load_schemaorg(schemaver))
+    type_properties = OrderedDict()
+    for schematype in s.mro():
+        type_properties[schematype] = schematype.propertiesIncludingAsDomain()
+    return type_properties
+
