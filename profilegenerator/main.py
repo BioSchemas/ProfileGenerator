d08cd38bbf1a1481580168e3546e74ee67f53d23
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2020-10-08 03:24:45 +0100

    Use schemaorg, tweak YAML

--- profilegenerator/main.py
+++ profilegenerator/main.py
@@ -24,7 +24,8 @@ from enum import IntEnum
 
 from ._version import __version__
 from ._logging import LOG_TRACE
-#from .schemaorg import find_properties
+from .schemaorg import SCHEMA, SchemaProperty, SchemaClass
+from . import schemaorg
 from .profileTemplate import profileHeader, profileProperty, profileFooter
 from .profileConstants import *
 
@@ -46,6 +47,29 @@ class Status(IntEnum):
     # User-specified exit codes
     # http://www.tldp.org/LDP/abs/html/exitcodes.html
     OTHER_ERROR = 166
+
+
+def _str_presenter(dumper, data):
+    if "\n" in data:
+        style='|'
+    elif '"' in data or ' ' in data:
+        style='"'
+    elif "'" in data:
+        style="'"
+    else:
+        style=''
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style=style)
+yaml.add_representer(str, _str_presenter)
+
+##
+
+## https://stackoverflow.com/a/41786451
+# CC BY-SA 4.0 by Jace Browning & Anthon
+def represent_none(self, _):
+    return self.represent_scalar('tag:yaml.org,2002:null', '')
+yaml.add_representer(type(None), represent_none)
+##
+
 
 
 def parse_args(args=None):
@@ -74,35 +98,73 @@ def parse_args(args=None):
         default="latest")
     return parser.parse_args(args)
 
-def generate(schematype, profileName=None, schemaver="latest", groupName=None, description=None):
+
+def make_example(s_type: SchemaClass, prop: SchemaProperty, 
+                 expectedType: SchemaClass) -> str:
+    example_id = "https://example.com/%s/123" % str(s_type).lower()
+    if not expectedType or expectedType.uri == SCHEMA.Text:
+        # Text - we do not know what it looks like; just use property name
+        exampleValue = '"example %s"' % str(prop).lower()
+    # Note: We'll only inspect the FIRST type in range
+    elif expectedType.uri == SCHEMA.URI:
+        # Some identifier - possibly related to property name
+        exampleValue = '"https://purl.example.org/%s-345"' % str(prop).lower()
+    elif expectedType.uri == SCHEMA.Thing:
+        # Unknown/any type - generic URI
+        exampleValue = '{"@id": "https://example.org/345"}'
+    else:
+        # Specified type of object
+        exampleValue = '{"@id": "https://example.com/%s/345", "@type": "%s"}' % (
+            str(expectedType).lower(), str(expectedType))
+
+    return '''{ "@context": "https://schema.org/",
+  "@id": "%s",
+  "@type": "%s",
+  "%s": %s
+}''' % (example_id, s_type, prop, exampleValue)
+
+def generate(schematype, profileName=None, groupName=None, description=None):
     """Generate bioschemas profile for a given schematype"""
-    assert schematype and schemaver
+    assert schematype
     profileName = profileName or schematype
-    groupName = groupName or profileName
+    groupName = groupName or profileName    
     
-    #props = find_properties(schematype, schemaver)
-    props = []
+
+    typ = schemaorg.find_class(schematype)
+    props = schemaorg.find_properties(schematype)
     
     ## TODO: Make yaml, template etc.
     _logger.info("Profile: %s" % profileName)
-    _logger.info("Based on schema.org: %s" % schemaver)
-    for (typ, properties) in props:
-        _logger.debug("Type: %s " % typ)
-        _logger.debug("Properties:")
-        for prop in properties:
-            _logger.debug("%s" % prop)
-    description = description or profileName ## TODO: From type
+    _logger.info("Based on schema.org %s version %s" % (typ, schemaorg.get_version()))
+    mappingProperies = []
+    for (s_type, s_props) in props.items():
+        _logger.debug("Type: %s " % s_type)
+        _logger.debug("Properties: %s", s_props)        
+        for prop in s_props:
+            propertyName = str(prop)
+            expectedTypes = [str(expected) for expected in prop.rangeIncludes]
+            schemaDescription = prop.comment or prop.label or str(prop)
+            bsDescription = 'TODO: Bioschemas description'
+            marginality = "TODO"
+            cardinality = "TODO"
+            controlledVocabs = ""
+            example = make_example(typ, prop, 
+                prop.rangeIncludes and prop.rangeIncludes[0])
+            _logger.info(example)
+            # TODO: record which s_type this property belongs to
+            mappingProperies.append(profileProperty(propertyName, expectedTypes, schemaDescription, 
+                bsDescription, marginality, cardinality, controlledVocabs, example))
+
+    description = description or typ.comment or profileName 
     # TODO: Schema version
     # TODO: Type hierarchy as dict with namespace and typename
     version = "0.1"
     status = STATUS_DRAFT
     profile = '---\n'
     profileDict = profileHeader(profileName, schematype, False, description, version, status, groupName, False)
-    mappingProperies = []
-    # TODO: Include mapping of property to domain ontology terms
-    mappingProperies.append(profileProperty('schemaPropertyName', ['type 1','type 2'], 'schema property description', 'Bioschemas description', MARGINALITY_UNSPECIFIED, None, None, None))
     profileDict['mapping'] = mappingProperies
-    profile += yaml.dump(profileDict)
+    _logger.debug(profileDict)
+    profile += yaml.dump(profileDict, default_flow_style=False, default_style='"', sort_keys=False)
     profile += '---\n'
     profile += profileFooter()
     writeToFile(profileName, version, status, profile)
@@ -139,8 +201,10 @@ def main(args=None):
         schematype = args.schematype
         assert schematype
         profileName = "profile" in args and args.profile or schematype
-        groupName = args.group or profileName        
-        return generate(schematype, profileName, args.schemaver, groupName, args.description)
+        groupName = args.group or profileName
+        schemaorg.set_version(args.schemaver)
+        return generate(schematype, profileName, groupName, args.description)
     except OSError as e:
         _logger.fatal(e)
         return Status.IO_ERROR
+
