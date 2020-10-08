7955c85bebff1d09dc8ea14256dbf437c550d770
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2020-10-08 10:54:22 +0100

    Better example types
    (Should this function move to schemaorg.py?)

--- profilegenerator/main.py
+++ profilegenerator/main.py
@@ -104,24 +104,39 @@ def parse_args(args=None):
 def make_example(s_type: SchemaClass, prop: SchemaProperty, 
                  expectedType: SchemaClass) -> str:
     example_id = "https://example.com/%s/123" % str(s_type).lower()
-    if not expectedType or expectedType.uri == SCHEMA.Text:
+    _logger.debug("Making example for [a %s] <%s> [a %s]" % (s_type, prop, expectedType))
+    if not expectedType or issubclass(expectedType, schemaorg.find_class(SCHEMA.Text)):
         # Text - we do not know what it looks like; just use property name
         exampleValue = '"example %s"' % str(prop).lower()
     # Note: We'll only inspect the FIRST type in range
-    elif expectedType.uri == SCHEMA.URL:
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.URL)):
         # Some identifier - possibly related to property name
         exampleValue = '"https://purl.example.org/%s-345"' % str(prop).lower()
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.Person)):
+        # Specified type of object
+        exampleValue = '{"@id": "https://orcid.org/0000-0002-1825-0097", "@type": "%s"}' % (
+            str(expectedType))            
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.Thing)):
+        # Specified type of object
+        exampleValue = '{"@id": "https://example.com/%s/345", "@type": "%s"}' % (
+            str(expectedType).lower(), str(expectedType))            
     elif expectedType.uri == SCHEMA.Thing:
         # Unknown/any type - generic object
         exampleValue = '{"@id": "https://example.org/345"}'
-    elif SCHEMA.Thing in expectedType.ancestors:
-        # Specified type of object
-        exampleValue = '{"@id": "https://example.com/%s/345", "@type": "%s"}' % (
-            str(expectedType).lower(), str(expectedType))
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.DateTime)):
+        exampleValue = '"2020-10-08T17:33:08+01:00"'
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.Date)):
+        exampleValue = '"2020-10-08"'
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.Time)):
+        exampleValue = '"17:33:08"'
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.Boolean)):
+        exampleValue = 'false'
+    elif issubclass(expectedType, schemaorg.find_class(SCHEMA.Number)):
+        exampleValue = '123'
     else:
         # Probably a datatype, fallback to empty string
         exampleValue = '""'
-
+    _logger.debug("Example value: %s" % exampleValue)
     return '''{ "@context": "https://schema.org/",
   "@id": "%s",
   "@type": "%s",
